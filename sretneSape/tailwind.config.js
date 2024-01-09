fcd0562eed97a1483c7a302b5368f504a0ba4894
Ana <apalac13>
2024-01-09 21:20:32 +0100

    added colors and fonts

--- sretneSape/tailwind.config.js
+++ sretneSape/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          45: "#1E1E1E",
+          46: "#231F20",
+        },
+        red: {
+          50: "#F81E1E",
+          51: "#4D1F21",
+          52: "#591212",
+        },
+        gray: {
+          60: "#E8E8E1",
+          61: "#D9D9D9",
+        },
+        white: {
+          70: "#FFFFFF",
+        },
+      },
+    },
   },
   plugins: [],
 };
