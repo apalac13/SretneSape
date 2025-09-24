352ce5c34092565e8013e583ca5878cd6736875d
apalac13 <anabradaric77@gmail.com>
2025-09-24 16:52:17 +0200

    fix nav

--- sretneSape/src/components/Navigacija.jsx
+++ sretneSape/src/components/Navigacija.jsx
@@ -1,25 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/slike/paws2.png";
+import { UserContext } from "../context/UserContext";
 
 function Navigacija({ action }) {
-  const user = useContext(useContext);
+  const user = useContext(UserContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className=" w-full flex justify-between  px-10 py-10  border-b-2 border-black-45 font-pt-sans-narrow  text-2xl text-black-45  ">
-      <div className=" w-full flex items-center justify-between">
-        <ul className=" hidden md:flex gap-[30px]   ">
+    <nav className="w-full flex justify-between px-10 py-10 border-b-2 border-black-45 font-pt-sans-narrow text-2xl text-black-45 relative">
+      {/* Logo */}
+      <NavLink to="/" className="w-8 h-8 md:hidden">
+        <img src={logo} alt="logo" />
+      </NavLink>
+
+      {/* Desktop menu */}
+      <div className="hidden md:flex w-full justify-between items-center">
+        <ul className="flex gap-[30px]">
           <li>
             <NavLink
-              to={"/"}
-              className=" text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
+              to="/"
+              className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               O NAMA
             </NavLink>
           </li>
           <li>
             <NavLink
-              to={"/popis"}
+              to="/popis"
               className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               POPIS
@@ -27,7 +35,7 @@ function Navigacija({ action }) {
           </li>
           <li>
             <NavLink
-              to={"/donacije"}
+              to="/donacije"
               className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               DONACIJE
@@ -35,7 +43,7 @@ function Navigacija({ action }) {
           </li>
           <li>
             <NavLink
-              to={"/obavijesti"}
+              to="/obavijesti"
               className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               OBAVIJESTI
@@ -43,53 +51,56 @@ function Navigacija({ action }) {
           </li>
           <li>
             <NavLink
-              to={"/unos"}
-              className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60 "
+              to="/unos"
+              className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               UNOS
             </NavLink>
           </li>
         </ul>
-        <ul className=" hidden md:flex items-center gap-[14px] hover:text-black-45/60">
-          <label htmlFor="admin" className=" cursor-pointer  ">
+
+        <div className="flex items-center gap-[14px]">
+          <label htmlFor="admin" className="cursor-pointer">
             ADMIN
           </label>
           <input
             id="admin"
-            alt="elipsa"
             type="checkbox"
             value={user}
             checked={user}
             onChange={action}
-            className="w-8 h-8 border border-solid border-black-45 rounded-full checked:bg-black-45 mr-2 cursor-pointer hover:black-45/60"
+            className="w-8 h-8 border border-solid border-black-45 rounded-full checked:bg-black-45 cursor-pointer"
           />
-        </ul>
+        </div>
       </div>
-      <NavLink to={"/"} className="w-8 h-8 absolute md:hidden ">
-        <img src={logo} alt="logo" />
-      </NavLink>
-      <button className="space-y-1 group md:hidden">
-        <div className="w-6 h-1  bg-black-45 "></div>
-        <div className="w-6 h-1 bg-black-45 "></div>
-        <div className="w-6 h-1 bg-black-45 "></div>
 
-        {/* menu */}
-        <ul className="z-10 w-screen bg-gray-61 pb-10 absolute -top-full group-focus:top-0 right-0 duration-150 flex flex-col space-y-3 justify-end">
-          <button className="px-10 py-8 relative ml-auto">
-            <div className="w-6 h-1 rotate-45 absolute bg-black-45"></div>
-            <div className="w-6 h-1 -rotate-45 absolute bg-black-45"></div>
-          </button>
+      {/* Mobile Hamburger */}
+      <button
+        onClick={() => setMenuOpen(!menuOpen)}
+        className="space-y-1 md:hidden flex flex-col justify-center"
+        aria-label="Menu"
+      >
+        <div className="w-6 h-1 bg-black-45"></div>
+        <div className="w-6 h-1 bg-black-45"></div>
+        <div className="w-6 h-1 bg-black-45"></div>
+      </button>
+
+      {/* Mobile menu */}
+      {menuOpen && (
+        <ul className="z-20 w-screen bg-gray-61 pb-10 absolute top-0 right-0 flex flex-col space-y-3 p-6">
           <li>
             <NavLink
-              to={"/"}
-              className=" text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
+              to="/"
+              onClick={() => setMenuOpen(false)}
+              className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               O NAMA
             </NavLink>
           </li>
           <li>
             <NavLink
-              to={"/popis"}
+              to="/popis"
+              onClick={() => setMenuOpen(false)}
               className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               POPIS
@@ -97,7 +108,8 @@ function Navigacija({ action }) {
           </li>
           <li>
             <NavLink
-              to={"/donacije"}
+              to="/donacije"
+              onClick={() => setMenuOpen(false)}
               className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               DONACIJE
@@ -105,7 +117,8 @@ function Navigacija({ action }) {
           </li>
           <li>
             <NavLink
-              to={"/obavijesti"}
+              to="/obavijesti"
+              onClick={() => setMenuOpen(false)}
               className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               OBAVIJESTI
@@ -113,28 +126,28 @@ function Navigacija({ action }) {
           </li>
           <li>
             <NavLink
-              to={"/unos"}
-              className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60 "
+              to="/unos"
+              onClick={() => setMenuOpen(false)}
+              className="text-black-45 hover:border-b-2 border-black-45/60 hover:text-black-45/60"
             >
               UNOS
             </NavLink>
           </li>
-          <li className="flex gap-2 items-center justify-center ">
-            <label htmlFor="admin" className=" cursor-pointer  ">
+          <li className="flex gap-2 items-center justify-center">
+            <label htmlFor="admin" className="cursor-pointer">
               ADMIN
             </label>
             <input
               id="admin"
-              alt="elipsa"
               type="checkbox"
               value={user}
               checked={user}
               onChange={action}
-              className=" w-8 h-8 max-[1022px]:w-6 max-[1022px]:h-6 border border-solid border-black-45 rounded-full checked:bg-black-45 mr-2 cursor-pointer hover:black-45/60"
+              className="w-8 h-8 border border-solid border-black-45 rounded-full checked:bg-black-45 cursor-pointer"
             />
           </li>
         </ul>
-      </button>
+      )}
     </nav>
   );
 }
